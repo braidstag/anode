0d8f8bcb8572d41daaed9fa406c332fb66992d86
NeoThermic <michaelkew@hotmail.com>
2019-08-16 21:00:16 +0100

    blanket update

--- templates/avrdude/flash.sh
+++ templates/avrdude/flash.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-
+gpio unexportall
 echo raise gpio pin to hold power on
 gpio -g mode 16 out
 gpio -g write 16 1
+echo flash fusebits
+sudo avrdude -p atmega328p -C /home/pi/braidstag/config/avrdude_gpio.conf -c pi_1 -U lfuse:w:0xFF:m
 echo flash firmware
-sudo avrdude -p atmega328p -C /braidstag/config/avrdude_gpio.conf -c pi_1 -v -U flash:w:/home/pi/arduino-milestag/IRcomms.ino.with_bootloader.hex:i
+sudo avrdude -p atmega328p -C /home/pi/braidstag/config/avrdude_gpio.conf -c pi_1 -v -U flash:w:/home/pi/braidstag/arduino-milestag/hex/IRcomms.ino.with_bootloader.hex:i
 echo Sleep
 sleep 10
 echo Drop pin
