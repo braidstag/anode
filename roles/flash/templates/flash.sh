dfb1c87a880e7156e9c5d322142600dbb0b642f9
Andrew Shirley <andrew@als-bi.com>
2021-02-13 22:53:43 +0000

    extract checkoutCodeRole, arduino-mk based flash
    + minor network tweaks, rename gamesetup to gamesetup-client

--- roles/flash/templates/flash.sh
+++ roles/flash/templates/flash.sh
@@ -1,13 +1,30 @@
-#!/bin/sh
-gpio unexportall
+#!/bin/sh -e
+
+cd /home/pi/braidstag/arduino-milestag/IRcomms
+echo Compile
+make
+
 echo raise gpio pin to hold power on
+gpio unexportall
 gpio -g mode 16 out
 gpio -g write 16 1
+
+export AVRDUDE_CONF=/home/pi/braidstag/config/avrdude_gpio.conf
+export AVRDUDE_ARD_PROGRAMMER=gpio
+export MONITOR_PORT=/dev/ttyS0
+
 echo flash fusebits
-sudo avrdude -p atmega328p -C /home/pi/braidstag/config/avrdude_gpio.conf -c pi_1 -U lfuse:w:0xFF:m
+#sudo avrdude -p atmega328p -C /home/pi/braidstag/config/avrdude_gpio.conf -c pi_1 -U lfuse:w:0xFF:m
+ISP_LOW_FUSE=0xff make set_fuses
+
 echo flash firmware
-sudo avrdude -p atmega328p -C /home/pi/braidstag/config/avrdude_gpio.conf -c pi_1 -v -U flash:w:/home/pi/braidstag/arduino-milestag/hex/IRcomms.ino.with_bootloader.hex:i
+#sudo avrdude -p atmega328p -C /home/pi/braidstag/config/avrdude_gpio.conf -c pi_1 -v -U flash:w:/home/pi/braidstag/arduino-milestag/hex/IRcomms.ino.with_bootloader.hex:i
+make upload
+
 echo Sleep
 sleep 10
-echo Drop pin
+
+echo Drop power pin
 gpio -g write 16 0
+
+echo Done
