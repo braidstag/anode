e99bc8a6a8e5b42b15c934db40fe0bbc32ba33af
Andrew Shirley <andrew@als-bi.com>
2021-02-13 22:55:16 +0000

    remove old files

--- hostchange.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ansibleDir=$(dirname $0)
-
-for ip in `cat ${ansibleDir}/server_list`; do
-  ansible-playbook -i $ip, hostname.yml --extra-vars "ansible_user=pi ansible_password=raspberry" --sudo
-
-done
